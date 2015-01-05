5707dbe0e4885899acdfa57033b50a9764998b8e
cPJerry <jerald@cpanel.net>
2015-01-05 01:39:18 -0600

    Update AutoLogins.user.js

--- AutoLogins.user.js
+++ AutoLogins.user.js
@@ -1,86 +1,176 @@
 // ==UserScript==
-// @name       Auto Logins
-// @version    1.4.6
-// @updateURL   https://raw.githubusercontent.com/cPJerry/Tweaks2/master/AutoLogins.user.js
-// @downloadURL https://raw.githubusercontent.com/cPJerry/Tweaks2/master/AutoLogins.user.js
-// @match      https://staff.whmcs.com/members/admin/supporttickets.php?action=viewticket&id=*
-// @match      https://staff.whmcs.com/members/admin/supporttickets.php?action=view&id=*
+// @name       WHMCS Ticket Tweaks
+// @require https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.js
+// @require https://cdnjs.cloudflare.com/ajax/libs/json2/20140204/json2.min.js
+// @match      https://staff.whmcs.com/members/admin/supporttickets.php?filter=1
+// @match	   https://staff.whmcs.com/members/admin/supporttickets.php
+// @match	   https://staff.whmcs.com/members/admin/supporttickets.php?view=&deptid=*&subject=&email=
+// @copyright  2014, Jerald Johnson
 // ==/UserScript==
+//alert(JSON.stringify(myArray, null, 2));
+if ($.cookie('hideTickets') == undefined) {
+	$.cookie('hideTickets', JSON.stringify(new Array()));
+}
 
-function str_replace(search, replace, subject, count) {
-  var i = 0,
-    j = 0,
-    temp = '',
-    repl = '',
-    sl = 0,
-    fl = 0,
-    f = [].concat(search),
-    r = [].concat(replace),
-    s = subject,
-    ra = Object.prototype.toString.call(r) === '[object Array]',
-    sa = Object.prototype.toString.call(s) === '[object Array]';
-  s = [].concat(s);
-  if (count) {
-    this.window[count] = 0;
-  }
+function in_array(needle, haystack, argStrict) {
+  //  discuss at: http://phpjs.org/functions/in_array/
+  // original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  // improved by: vlado houba
+  // improved by: Jonas Sciangula Street (Joni2Back)
+  //    input by: Billy
+  // bugfixed by: Brett Zamir (http://brett-zamir.me)
+  //   example 1: in_array('van', ['Kevin', 'van', 'Zonneveld']);
+  //   returns 1: true
+  //   example 2: in_array('vlado', {0: 'Kevin', vlado: 'van', 1: 'Zonneveld'});
+  //   returns 2: false
+  //   example 3: in_array(1, ['1', '2', '3']);
+  //   example 3: in_array(1, ['1', '2', '3'], false);
+  //   returns 3: true
+  //   returns 3: true
+  //   example 4: in_array(1, ['1', '2', '3'], true);
+  //   returns 4: false
 
-  for (i = 0, sl = s.length; i < sl; i++) {
-    if (s[i] === '') {
-      continue;
+  var key = '',
+    strict = !! argStrict;
+
+  //we prevent the double check (strict && arr[key] === ndl) || (!strict && arr[key] == ndl)
+  //in just one for, in order to improve the performance 
+  //deciding wich type of comparation will do before walk array
+  if (strict) {
+    for (key in haystack) {
+      if (haystack[key] === needle) {
+        return true;
+      }
     }
-    for (j = 0, fl = f.length; j < fl; j++) {
-      temp = s[i] + '';
-      repl = ra ? (r[j] !== undefined ? r[j] : '') : r[0];
-      s[i] = (temp)
-        .split(f[j])
-        .join(repl);
-      if (count && s[i] !== temp) {
-        this.window[count] += (temp.length - s[i].length) / f[j].length;
+  } else {
+    for (key in haystack) {
+      if (haystack[key] == needle) {
+        return true;
       }
     }
   }
-  return sa ? s : s[0];
-}
-var style = "display:inline;";
-var insertAt = $('body').find('#customfield134').parent().parent().parent().parent().parent().find('div');
 
-var cpanelPass = $('#customfield166').attr('value');
-var cpanelUser = $('#customfield162').attr('value');
-var cpanelHost = $('#customfield158').attr('value');
-
-cpanelHost = str_replace("https://","",cpanelHost);
-cpanelHost = str_replace("http://","",cpanelHost);
-cpanelHost = str_replace(":2082","",cpanelHost);
-cpanelHost = str_replace(":2083","",cpanelHost);
-cpanelHost = cpanelHost.replace(/\/(.*)/g,"");
-
-var logincPanel = "<form style='"+style+"' target='_blank' name='cpanelform' action='https://"+cpanelHost+":2083/login' method='post'><input type='hidden' name='user' value='"+cpanelUser+"'><input type='hidden' name='pass' value='"+cpanelPass+"'><input type='submit' value='Login to cPanel'></form>ABC";
-$(logincPanel).appendTo(insertAt);
-var https = 0;
-var whmcsPass = $('#customfield142').attr('value');
-var whmcsUser = $('#customfield138').attr('value');
-var whmcsHost = $('#customfield134').attr('value');
-var whmcsHost2 = whmcsHost;
-
-whmcsHost = str_replace("https://","",whmcsHost);
-var proto = "http://";
-if (whmcsHost != whmcsHost2) {
-    proto = "https://";
-}
-whmcsHost = str_replace("/index.php","",whmcsHost);
-whmcsHost = str_replace("/login.php","",whmcsHost);
-whmcsHost = whmcsHost+"/dologin.php";
-whmcsHost = str_replace("//dologin.php","/dologin.php",whmcsHost);
-
-
-
-whmcsHost = str_replace("http://","",whmcsHost);
-
-if (cpanelHost == ".HTACCESS") {
-    whmcsHost = proto+encodeURIComponent(cpanelUser)+":"+encodeURIComponent(cpanelPass)+"@"+whmcsHost;
-} else {
-    whmcsHost = proto+whmcsHost;
+  return false;
 }
 
-var loginWhmcs = "<form target='_blank' style='"+style+"' name='whmcsform' action='"+whmcsHost+"' method='post'><input type='hidden' name='username' value='"+whmcsUser+"'><input type='hidden' name='password' value='"+whmcsPass+"'><input type='submit' value='Login to WHMCS'></form>ABC";
-$(loginWhmcs).appendTo(insertAt);
+var myname = "jerald";
+function getQueue(queue) {
+    if (queue == null) return '';
+    var queue2 = queue.replace(/(.*) \((.*)\)/,"$1");
+    if (queue2 == queue) return queue;
+    return queue2;
+}
+
+function hideTicket(item) {
+    item.parent().hide();
+}
+
+function getHandler(queue,me) {
+    if (me == "sortabletbl2") return "Jerald";
+    if (queue == null) return '';
+    var queue2 = queue.replace(/(.*) \((.*)\)/,"$2");
+    if (queue2 == queue) return "Unassigned";
+    return queue2;
+}
+function getTicketID(data) {
+    return data.replace(/#(.*) - (.*)/,"$1");
+}
+                        
+function isOnline(user) {
+    user = user.replace(/(.*) (.*)/,"$1");
+    if (user == "Samuel") user = "scraven";
+    user = user.toLowerCase();
+    var userList = '';
+    $('#sidebar').children('div').each(function() {
+        var tmp = $(this).html();
+        if (tmp.toLowerCase().indexOf("jerald") >= 0) {
+            userList = tmp.toLowerCase();
+        }
+    });
+    if (userList.indexOf(user.toLowerCase()) >= 0) { return 1;}
+    return 0;
+}
+var disableJesse = 1;
+var disableChris = 1;
+var disableMatt = 1;
+var disableBugs = 1;
+var disableL2 = 1;
+var disableLicensing = 0;
+$.cookie.json = true;
+function isMyTKT(whose,queue) {
+	
+    if (queue != null && whose != "sortabletbl1") {
+        if (queue == "Bugs" && disableBugs == 1) return false;
+        if (queue == "Level 2 Support" && disableL2 == 1) return false;
+        if (queue == "Licensing" && disableLicensing == 1) return false;
+
+    }
+    if (queue == null) {
+        if (whose == "Matt" && disableMatt) return false;
+        if (whose == "Chris Borsheim" && disableChris) return false;
+        if (whose == "Jesse Asklund" && disableJesse) return false;
+    }
+    return true;
+	
+}
+
+function magic(type2) {
+    var TktCount = 0;
+    var pri = $('[id="'+type2+'"]');
+    window.data = pri;
+    pri.addClass( "ticketsLink" );
+    for (var i=0;i<$('[id="'+type2+'"] tr').length+1;i++) {
+        
+        if (pri.find('tr:nth-child('+i+') td:nth-child(2)').html() == null) {
+            pri.find('tr:nth-child('+i+') th:nth-child(2)').html("Handler");
+            pri.find('tr:nth-child('+i+') th:nth-child(2)').css
+        }
+        var status = getHandler(pri.find('tr:nth-child('+i+') td:nth-child(3)').html(),type2)+"<br />(Offline)";
+        if (isOnline(getHandler(pri.find('tr:nth-child('+i+') td:nth-child(3)').html(),type2))) {
+            status = getHandler(pri.find('tr:nth-child('+i+') td:nth-child(3)').html(),type2)+"<br />(Online)";
+        } else {
+            pri.find('tr:nth-child('+i+')').css( "color", "red" );//("background-color","gray");
+        }
+		if (!isMyTKT(getHandler(pri.find('tr:nth-child('+i+') td:nth-child(3)').html(),type2),null)
+        //    getHandler(pri.find('tr:nth-child('+i+') td:nth-child(3)').html(),type2) == "Jesse Asklund"
+        )
+        	pri.find('tr:nth-child('+i+')').hide();
+        pri.find('tr:nth-child('+i+') td:nth-child(2)').css('width','100px');
+        pri.find('tr:nth-child('+i+') td:nth-child(2)').html(status);
+        pri.find('tr:nth-child('+i+') td:nth-child(3)').html(getQueue(pri.find('tr:nth-child('+i+') td:nth-child(3)').html()));
+        if (!isMyTKT(type2,getQueue(pri.find('tr:nth-child('+i+') td:nth-child(3)').html()))) {
+            pri.find('tr:nth-child('+i+')').hide();
+        } else {
+            pri.find('tr:nth-child('+i+') td:nth-child(4)').find("a").each(function(){
+                var id = getTicketID($(this).html());
+                var cookies2 = $.cookie("hideTickets");
+                if (in_array(id,cookies2)) {
+                    $(this).parent().parent().hide();
+                    TktCount--;
+                } else {
+                    $(this).parent().html($(this).parent().html()+" <span style='border: 3px; color: red; text-decoration: underline;' href='#' action='javscript:void;' onclick='if(confirm(\"Hide this ticket? Really?\")) {var cookies = $.cookie(\"hideTickets\"); cookies.push(\""+id+"\");$.cookie(\"hideTickets\",cookies);$(this).parent().parent().hide();}'>Hide This Ticket</span>");
+                }
+        });
+            if (getQueue(pri.find('tr:nth-child('+i+') td:nth-child(3)').html()) != "" && getHandler(pri.find('tr:nth-child('+i+') td:nth-child(3)').html()) != "") {
+	            TktCount++;
+
+            }
+        }
+        
+        
+    }
+    if (type2 == "sortabletbl3") {
+		$('[action="/members/admin/supporttickets.php?filter=1"]').children().find("td").each(function(){
+  	      if ($(this).attr('align') == "left") {
+            $(this).html(TktCount+" Records Found");
+          }
+    	});
+    }
+    
+    
+}
+magic("sortabletbl1");
+magic("sortabletbl2");
+magic("sortabletbl3");
+
+
