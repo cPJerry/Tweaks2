b725df13cbbf8bb52a205a5516c3fac07b364f91
cPJerry <jerald@cpanel.net>
2015-01-14 22:51:36 -0600

    Update InTicketTweaks.user.js

--- InTicketTweaks.user.js
+++ InTicketTweaks.user.js
@@ -6,7 +6,42 @@
 // @match      https://staff.whmcs.com/members/admin/supporttickets.php?action=view*&id=*
 // @copyright  2012+, You
 // ==/UserScript==
+function urlencode(str) {
+  //       discuss at: http://phpjs.org/functions/urlencode/
+  //      original by: Philip Peterson
+  //      improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  //      improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  //      improved by: Brett Zamir (http://brett-zamir.me)
+  //      improved by: Lars Fischer
+  //         input by: AJ
+  //         input by: travc
+  //         input by: Brett Zamir (http://brett-zamir.me)
+  //         input by: Ratheous
+  //      bugfixed by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  //      bugfixed by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  //      bugfixed by: Joris
+  // reimplemented by: Brett Zamir (http://brett-zamir.me)
+  // reimplemented by: Brett Zamir (http://brett-zamir.me)
+  //             note: This reflects PHP 5.3/6.0+ behavior
+  //             note: Please be aware that this function expects to encode into UTF-8 encoded strings, as found on
+  //             note: pages served as UTF-8
+  //        example 1: urlencode('Kevin van Zonneveld!');
+  //        returns 1: 'Kevin+van+Zonneveld%21'
+  //        example 2: urlencode('http://kevin.vanzonneveld.net/');
+  //        returns 2: 'http%3A%2F%2Fkevin.vanzonneveld.net%2F'
+  //        example 3: urlencode('http://www.google.nl/search?q=php.js&ie=utf-8&oe=utf-8&aq=t&rls=com.ubuntu:en-US:unofficial&client=firefox-a');
+  //        returns 3: 'http%3A%2F%2Fwww.google.nl%2Fsearch%3Fq%3Dphp.js%26ie%3Dutf-8%26oe%3Dutf-8%26aq%3Dt%26rls%3Dcom.ubuntu%3Aen-US%3Aunofficial%26client%3Dfirefox-a'
 
+  str = (str + '')
+    .toString();
+
+  // Tilde should be allowed unescaped in future versions of PHP (as reflected below), but if you want to reflect current
+  // PHP behavior, you would need to add ".replace(/~/g, '%7E');" to the following.
+  return encodeURIComponent(str)
+    .replace(/!/g, '%21')
+    .replace(/'/g, '%27')
+
+}
 
 
 $('.reply').css('background-color','#D3ECD7');
@@ -34,4 +69,21 @@ $('input').blur(function() {
         $(this).attr('size','30');
         $(this).css('background-color','#ffffff');
     }
+});
+
+var subject = "Unknown";
+$('#contentarea').find('h2').each(function() {
+	var data = $(this).html().replace(/(.*)<select(.*)/,"$1");
+	data = data.split("\n");
+	subject = data[0];
+	return false;
+});
+$('.note').children('.rightcol').children('.postedon').each(function() {
+    var escalate = "<a href='mailto:techs@whmcs.com?subject="+urlencode(subject)+"&body="+urlencode(window.location+"\n\n");
+    var body = "UNKNOWN - REPLACE ME!";
+    $(this).parent().children('.msgwrap').children('.message').each(function() {
+		body = $(this).html();
+	});
+    escalate = escalate + urlencode(body) + "'>Escalate</a>";
+	$(this).html($(this).html()+"&nbsp; "+escalate);
 });
